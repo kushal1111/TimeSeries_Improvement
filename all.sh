89e144b10a2e34701c3fe089ae8eada72f232baf
kushal1111 <kushal.adhikari@g-mana.com>
2025-03-24 10:19:24 +0545

    add shap

--- all.sh
+++ all.sh
@@ -1,13 +1,7 @@
-bash ./scripts/long_term_forecast/ETT_script/TimeLinear_ETTh1.sh
+bash ./scripts/long_term_forecast/ETT_script/TimeModernTCN_ETTh1.sh
 
-# bash ./scripts/long_term_forecast/ETT_script/TimeLinear_ETTh2.sh
+bash ./scripts/long_term_forecast/ETT_script/TimeModernTCN_ETTh2.sh
 
-# bash ./scripts/long_term_forecast/ETT_script/TimeLinear_ETTm1.sh
+bash ./scripts/long_term_forecast/ETT_script/TimeModernTCN_ETTm1.sh
 
-# bash ./scripts/long_term_forecast/ETT_script/TimeLinear_ETTm2.sh
-
-# bash ./scripts/long_term_forecast/Weather_script/TimeLinear.sh
-
-# bash ./scripts/long_term_forecast/Traffic_script/TimeLinear.sh
-
-# bash ./scripts/long_term_forecast/ECL_script/TimeLinear.sh
+bash ./scripts/long_term_forecast/ETT_script/TimeModernTCN_ETTm2.sh
